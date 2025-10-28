6b15f208d8ca6bd24de2da4e2f88fdc7e60194d0
PaiVarnalt <rfqialhkm@gmail.com>
2025-10-28 14:27:07 +0700

    rifqi

--- db/db_bioskop.sql
+++ db/db_bioskop.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Waktu pembuatan: 13 Okt 2025 pada 06.59
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.2.12
+-- Host: localhost
+-- Generation Time: Oct 28, 2025 at 07:22 AM
+-- Server version: 8.0.17
+-- PHP Version: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,25 +25,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `film`
+-- Table structure for table `film`
 --
 
 CREATE TABLE `film` (
   `id_film` int(11) NOT NULL,
-  `judul` varchar(100) NOT NULL,
-  `genre` varchar(50) DEFAULT NULL,
+  `judul` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `genre` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `durasi` int(11) DEFAULT NULL,
   `tanggal_rilis` date DEFAULT NULL,
-  `status` enum('tayang','populer','baru','coming') DEFAULT 'tayang',
+  `status` enum('tayang','populer','baru','coming') COLLATE utf8mb4_general_ci DEFAULT 'tayang',
   `harga` decimal(10,2) DEFAULT NULL,
-  `gambar` varchar(255) DEFAULT NULL,
-  `banner` varchar(255) DEFAULT NULL
+  `gambar` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `banner` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `film`
+--
+
+INSERT INTO `film` (`id_film`, `judul`, `genre`, `durasi`, `tanggal_rilis`, `status`, `harga`, `gambar`, `banner`) VALUES
+(1, 'Ganang 1991', 'romansa, aksi', 5, '2025-10-22', 'coming', '25000.00', 'asset/img/film/ganang1991.jpg', 'asset/img/banner/bannerganang1991.jpg'),
+(3, 'aaa', 'romansa, aksi', 0, '2025-10-21', 'tayang', '35000.00', 'asset/img/film/banner.webp', 'asset/img/banner/banner.webp');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `jadwal_tayang`
+-- Table structure for table `jadwal_tayang`
 --
 
 CREATE TABLE `jadwal_tayang` (
@@ -50,25 +59,41 @@ CREATE TABLE `jadwal_tayang` (
   `id_film` int(11) NOT NULL,
   `tanggal` date NOT NULL,
   `jam` time NOT NULL,
-  `studio` varchar(50) DEFAULT NULL
+  `id_studio` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `jadwal_tayang`
+--
+
+INSERT INTO `jadwal_tayang` (`id_jadwal`, `id_film`, `tanggal`, `jam`, `id_studio`) VALUES
+(1, 1, '2025-10-22', '09:15:00', 1);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kursi`
+-- Table structure for table `kursi`
 --
 
 CREATE TABLE `kursi` (
   `id_kursi` int(11) NOT NULL,
-  `studio` varchar(50) DEFAULT NULL,
-  `nomor_kursi` varchar(10) DEFAULT NULL
+  `id_studio` int(11) DEFAULT NULL,
+  `nomor_kursi` varchar(10) COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kursi`
+--
+
+INSERT INTO `kursi` (`id_kursi`, `id_studio`, `nomor_kursi`) VALUES
+(1, 1, 'A1'),
+(2, 1, 'A2'),
+(3, 1, 'A3');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `reservasi`
+-- Table structure for table `reservasi`
 --
 
 CREATE TABLE `reservasi` (
@@ -76,61 +101,88 @@ CREATE TABLE `reservasi` (
   `id_user` int(11) NOT NULL,
   `id_jadwal` int(11) NOT NULL,
   `id_kursi` int(11) NOT NULL,
-  `tanggal_reservasi` datetime DEFAULT current_timestamp(),
-  `status` enum('pending','dibayar','batal') DEFAULT 'pending',
+  `tanggal_reservasi` datetime DEFAULT CURRENT_TIMESTAMP,
+  `status` enum('pending','dibayar','batal') COLLATE utf8mb4_general_ci DEFAULT 'pending',
   `total_harga` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `studio`
+--
+
+CREATE TABLE `studio` (
+  `id_studio` int(11) NOT NULL,
+  `nama_studio` varchar(100) NOT NULL,
+  `tipe_layar` varchar(50) DEFAULT NULL,
+  `keterangan` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `studio`
+--
+
+INSERT INTO `studio` (`id_studio`, `nama_studio`, `tipe_layar`, `keterangan`) VALUES
+(1, 'studio 1', '2d', ''),
+(2, 'studio 2', '2d', ''),
+(3, 'studio 3', '2d', ''),
+(4, 'studio 4', '2d', ''),
+(5, 'studio 5', '3d', ''),
+(6, 'studio 6', 'IMAX', ''),
+(7, 'studio 7', 'IMAX', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `foto_profil` varchar(255) DEFAULT NULL,
-  `role` enum('admin','user') DEFAULT 'user',
-  `tanggal_daftar` datetime DEFAULT current_timestamp()
+  `username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `foto_profil` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `role` enum('admin','user') COLLATE utf8mb4_general_ci DEFAULT 'user',
+  `tanggal_daftar` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`user_id`, `username`, `password`, `email`, `foto_profil`, `role`, `tanggal_daftar`) VALUES
-(1, 'iki', '$2y$10$e86dfJC4OHlqsxckhl3QtuzPU37sgnSDqkqB/GO7aevZ8d0OdwONO', 'admin@gmail', '68ea86d979834.png', 'user', '2025-10-11 23:33:29'),
-(2, 'iki', '$2y$10$dSZNFCcDSEkA9CWDGXATpu/pTrSlOO9h8IB3IymIANY5Q/e1FaPBa', 'admin@gmail', '68ea8767c578d.png', 'user', '2025-10-11 23:35:51'),
-(3, 'iki', '$2y$10$ZX8vBGCafLaLUF9IMdhsn.sT2nQ57bErmZ08Vjr4Y95g1dDZBH3WG', 'admin@gmail', '68ea88f2b2a0c.jpg', 'user', '2025-10-11 23:42:26');
+(8, 'rifqi', '$2y$10$puzeZH8S3xAVlIhkjSdpGOOeUvts16aro2Xtr0oOiDcSyIOWMl2y.', 'rifqigmrpl@gmail.com', 'asset/img/user/logo.png', 'admin', '2025-10-19 23:21:57'),
+(9, 'faizi', '$2y$10$ZPAdnDcgROiJXPTzspigluaLLKx1RFJKNKsd.3FNCaQEU8LS8D8ja', 'faizi@gmail.com', 'asset/img/user/logo.png', 'user', '2025-10-20 18:32:27');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `film`
+-- Indexes for table `film`
 --
 ALTER TABLE `film`
   ADD PRIMARY KEY (`id_film`);
 
 --
--- Indeks untuk tabel `jadwal_tayang`
+-- Indexes for table `jadwal_tayang`
 --
 ALTER TABLE `jadwal_tayang`
   ADD PRIMARY KEY (`id_jadwal`),
-  ADD KEY `id_film` (`id_film`);
+  ADD KEY `id_film` (`id_film`),
+  ADD KEY `studio` (`id_studio`);
 
 --
--- Indeks untuk tabel `kursi`
+-- Indexes for table `kursi`
 --
 ALTER TABLE `kursi`
-  ADD PRIMARY KEY (`id_kursi`);
+  ADD PRIMARY KEY (`id_kursi`),
+  ADD KEY `id_studio` (`id_studio`);
 
 --
--- Indeks untuk tabel `reservasi`
+-- Indexes for table `reservasi`
 --
 ALTER TABLE `reservasi`
   ADD PRIMARY KEY (`id_reservasi`),
@@ -139,57 +191,76 @@ ALTER TABLE `reservasi`
   ADD KEY `id_kursi` (`id_kursi`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `studio`
+--
+ALTER TABLE `studio`
+  ADD PRIMARY KEY (`id_studio`);
+
+--
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `film`
+-- AUTO_INCREMENT for table `film`
 --
 ALTER TABLE `film`
-  MODIFY `id_film` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_film` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `jadwal_tayang`
+-- AUTO_INCREMENT for table `jadwal_tayang`
 --
 ALTER TABLE `jadwal_tayang`
-  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `kursi`
+-- AUTO_INCREMENT for table `kursi`
 --
 ALTER TABLE `kursi`
-  MODIFY `id_kursi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kursi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `reservasi`
+-- AUTO_INCREMENT for table `reservasi`
 --
 ALTER TABLE `reservasi`
   MODIFY `id_reservasi` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `studio`
+--
+ALTER TABLE `studio`
+  MODIFY `id_studio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `jadwal_tayang`
+-- Constraints for table `jadwal_tayang`
 --
 ALTER TABLE `jadwal_tayang`
-  ADD CONSTRAINT `jadwal_tayang_ibfk_1` FOREIGN KEY (`id_film`) REFERENCES `film` (`id_film`) ON DELETE CASCADE;
+  ADD CONSTRAINT `jadwal_tayang_ibfk_1` FOREIGN KEY (`id_film`) REFERENCES `film` (`id_film`) ON DELETE CASCADE,
+  ADD CONSTRAINT `jadwal_tayang_ibfk_2` FOREIGN KEY (`id_studio`) REFERENCES `studio` (`id_studio`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
--- Ketidakleluasaan untuk tabel `reservasi`
+-- Constraints for table `kursi`
+--
+ALTER TABLE `kursi`
+  ADD CONSTRAINT `kursi_ibfk_1` FOREIGN KEY (`id_studio`) REFERENCES `studio` (`id_studio`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `reservasi`
 --
 ALTER TABLE `reservasi`
   ADD CONSTRAINT `reservasi_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`user_id`) ON DELETE CASCADE,
